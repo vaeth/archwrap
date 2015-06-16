755892924b56743dd99e58465466309b33b3a798
Martin Väth <martin@mvath.de>
2015-06-16 20:15:54 +0200

    Remove redundant sh quoting

--- bin/archwrap.sh
+++ bin/archwrap.sh
@@ -4,56 +4,56 @@
 
 which=`command -v which` || which=
 MakeExternal() {
-	[ "${which}" = 'which' ] && eval ${1}=\`which ${2}\` \
-		|| eval ${1}=\`command -v ${2}\` || eval ${1}=
-	eval ": \${${1}:=${2}}"
+	[ "$which" = 'which' ] && eval $1=\`which $2\` \
+		|| eval $1=\`command -v $2\` || eval $1=
+	eval ": \${$1:=$2}"
 }
 
 OptExternal() {
-	eval "[ -n \"\${${1}:++}\" ]" || MakeExternal ${1} ${2-${1}}
+	eval "[ -n \"\${$1:++}\" ]" || MakeExternal $1 ${2-$1}
 }
 
 Echo() {
-	printf '%s\n' "${*}"
+	printf '%s\n' "$*"
 }
 
 ErrMessage() {
-	Echo "${0##*/}: ${*}" >&2
+	Echo "${0##*/}: $*" >&2
 }
 
 retvalue=0
 Error() {
 	e='. '
-	[ "${1}" -gt "${retvalue}" ] && retvalue=${1}
+	[ "$1" -gt "$retvalue" ] && retvalue=$1
 	shift
-	case ${*} in
+	case $* in
 	*'.')	e=' ';;
 	*' '|*'
 ')	e=;;
 	esac
-	if ${errbreak}
-	then	ErrMessage "${*}${e}Stopped."
-		exit ${retvalue}
-	else	ErrMessage "${*}${e}(proceeding)"
-		return ${retvalue}
+	if $errbreak
+	then	ErrMessage "$*$eStopped."
+		exit $retvalue
+	else	ErrMessage "$*$e(proceeding)"
+		return $retvalue
 	fi
 }
 
 Exit() {
-	[ ${retvalue} -eq 0 ] && exit
+	[ $retvalue -eq 0 ] && exit
 	ErrMessage 'Proceeded despite earlier errors.'
-	exit ${retvalue}
+	exit $retvalue
 }
 
 Push() {
 	. push.sh
-	Push "${@}"
+	Push "$@"
 }
 
 tempname=
 RmTemp() {
 	trap : EXIT HUP INT TERM
-	[ -n "${tempname:++}" ] && rm -f -- "${tempname}"
+	[ -n "${tempname:++}" ] && rm -f -- "$tempname"
 	tempname=
 	trap - EXIT HUP INT TERM
 }
@@ -65,43 +65,43 @@ MkTemp() {
 			havemktemp=: || havemktemp=false
 	fi
 	trap RmTemp EXIT HUP INT TERM
-	if ${havemktemp}
+	if $havemktemp
 	then	tempname=`mktemp "/tmp/${0##*/}.XXXXXXXX"` && \
 			[ -n "${tempname:++}" ] && return
 		ErrMessage 'cannot create temporary file'
 		return 2
 	fi
 	if [ -z "${have_random:++}" ]
-	then	r=${RANDOM}
-		if [ "${r}" = "${RANDOM}" ] && \
-			[ "${r}" = "${RANDOM}" ]
+	then	r=$RANDOM
+		if [ "$r" = "$RANDOM" ] && \
+			[ "$r" = "$RANDOM" ]
 		then	have_random=false
 			r=`od -d -N2 /dev/random 2>/dev/null` || r=
-			r=`printf '%s' ${r}`
+			r=`printf '%s' $r`
 			if [ -z "${r:++}" ]
 			then	r=1
-			else	r=$(( ${r} % 32768 ))
-				[ "${r}" -eq 0 ] && r=1
+			else	r=$(( $r % 32768 ))
+				[ "$r" -eq 0 ] && r=1
 			fi
 		else	have_random=:
 		fi
 		t=
 	fi
 	c=0
-	while [ ${c} -le 999 ]
+	while [ $c -le 999 ]
 	do	if [ -n "${t:++}" ]
-		then	if ${have_random}
-			then	r=${RANDOM}
-			else	r=$(( ${r} * 13821 ))
-				r=$(( ${r} % 32768 ))
+		then	if $have_random
+			then	r=$RANDOM
+			else	r=$(( $r * 13821 ))
+				r=$(( $r % 32768 ))
 			fi
 		fi
-		t="/tmp/${0##*/}.${$}${c}${r}"
+		t=/tmp/${0##*/}.$$$c$r
 		(
 			set -C
-			: >"${t}"
-		) && tempname=${t} && return
-		c=$(( ${c} + 1 ))
+			: >"$t"
+		) && tempname=$t && return
+		c=$(( $c + 1 ))
 	done
 	ErrMessage 'cannot create temporary file'
 	return 2
@@ -109,9 +109,9 @@ MkTemp() {
 
 
 Cd() {
-	case ${1} in
-	/*)	cd "${1}" >/dev/null 2>&1 || Error 2 "cd ${1} failed";;
-	*)	cd "./${1#./}" >/dev/null 2>&1 || Error 2 "cd ${PWD}/${1./} failed";;
+	case $1 in
+	/*)	cd "$1" >/dev/null 2>&1 || Error 2 "cd $1 failed";;
+	*)	cd "./${1#./}" >/dev/null 2>&1 || Error 2 "cd $PWD/${1./} failed";;
 	esac
 }
 
@@ -119,10 +119,10 @@ PushTopack() {
 	Push -c topack
 	set +f
 	for topacki in .* *
-	do	case ${topacki} in
+	do	case $topacki in
 		.|..)	continue;;
 		esac
-		test -r "${topacki}" && Push topack "${topacki}"
+		test -r "$topacki" && Push topack "$topacki"
 	done
 	set -f
 	[ -n "${topack:++}" ]
